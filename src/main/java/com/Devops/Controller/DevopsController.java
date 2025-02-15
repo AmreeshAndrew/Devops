7ab792dbfdbbb2ee8b90cf21747119470a006cf4
Your Name <you@example.com>
2025-02-15 14:31:46 +0530

    secondcommit

--- src/main/java/com/Devops/Controller/DevopsController.java
+++ src/main/java/com/Devops/Controller/DevopsController.java
@@ -1,5 +1,14 @@
 package com.Devops.Controller;
 
-public class DevopsController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping(value="/devops")
+public class DevopsController {
+@GetMapping(value="/get")
+public String Get() {
+	return "Hi";
+}
 }
